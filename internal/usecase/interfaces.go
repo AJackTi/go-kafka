755feda0341dbea83583e7c4a737efb4834ccabc
AJackTi <dtrong97vn@gmail.com>
2023-09-07 21:05:07 +0700

    chore: remove unused file

--- internal/usecase/interfaces.go
+++ internal/usecase/interfaces.go
@@ -10,12 +10,6 @@ import (
 //go:generate mockgen -source=interfaces.go -destination=./mocks_test.go -package=usecase_test
 
 type (
-	// Translation -.
-	Translation interface {
-		Translate(context.Context, entity.Translation) (entity.Translation, error)
-		History(context.Context) ([]entity.Translation, error)
-	}
-
 	// Task
 	Task interface {
 		CreateTask(context.Context, *CreateTaskRequest) error
@@ -25,12 +19,6 @@ type (
 		// Delete(context.Context, string) error
 	}
 
-	// TranslationRepo -.
-	TranslationRepo interface {
-		Store(context.Context, entity.Translation) error
-		GetHistory(context.Context) ([]entity.Translation, error)
-	}
-
 	// TaskRepo -.
 	TaskRepo interface {
 		CreateTask(context.Context, *entity.Task) error
@@ -38,10 +26,5 @@ type (
 		// Get(context.Context, string) (*entity.Task, error)
 		// Update(context.Context, string, *entity.Task) error
 		// Delete(context.Context, string) error
-	}
-
-	// TranslationWebAPI -.
-	TranslationWebAPI interface {
-		Translate(entity.Translation) (entity.Translation, error)
 	}
 )
